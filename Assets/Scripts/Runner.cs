34415f16eb60aef508e3d626f2800e9a29552f60
JeongHwiSeok <jhsang1996@gmail.com>
2024-01-11 19:46:42 +0900

    (2024.01.11) 입력 시스템 추가 및 이동 시스템 추가

--- Assets/Scripts/Runner.cs
+++ Assets/Scripts/Runner.cs
@@ -13,6 +13,10 @@ public class Runner : MonoBehaviour
 {
     [SerializeField] RoadLine roadLine;
 
+    [SerializeField] public float maxWidth = 3.5f;
+
+    [SerializeField] public float speed = 5f;
+
     private void Start()
     {
         roadLine = RoadLine.MIDDLE;
@@ -27,18 +31,41 @@ public class Runner : MonoBehaviour
     {
         if(Input.GetKeyDown(KeyCode.LeftArrow))
         {
-            if(roadLine >= RoadLine.MIDDLE)
+            if(roadLine > RoadLine.LEFT)
             {
                 roadLine--;
+                Status();
             }
         }
         
         if(Input.GetKeyDown(KeyCode.RightArrow))
         {
-            if(roadLine <= RoadLine.MIDDLE)
+            if(roadLine < RoadLine.RIGHT)
             {
                 roadLine++;
+                Status();
             }
         }
+    }
+
+    private void Status()
+    {
+        switch(roadLine)
+        {
+            case RoadLine.LEFT:
+                Movement(-maxWidth);
+                break;
+            case RoadLine.MIDDLE:
+                Movement(0);
+                break;
+            case RoadLine.RIGHT:
+                Movement(maxWidth);
+                break;
+        }
+    }
+
+    private void Movement(float maxWidth)
+    {
+        transform.position = new Vector3(maxWidth, 0, 0);
     }
 }
